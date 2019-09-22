9d4d444e61741e9c030defa32e46c824b360c51b
Juhi Rani <Juhi.Rani@sumtotalsystems.com>
2019-09-22 13:29:40 +0530

    m

---
+++ src/test/java/SumOfTwoNumbers.java
@@ -0,0 +1,13 @@
+public class SumOfTwoNumbers {
+
+
+    public void addTwoNumbers(int a, int b){
+        int sum=a+b;
+        System.out.println("The sum is : "+sum);
+
+    }
+    public static void main(String args[]){
+        SumOfTwoNumbers su=new SumOfTwoNumbers();
+        su.addTwoNumbers(2,5);
+    }
+}
